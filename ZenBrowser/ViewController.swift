084aa6f06b94564fb835c74b7d5dd49ee6320efd
Kubilay Erdogan's MBPR <kubilay.erdogan@frame.com.tr>
2015-05-13 11:45:26 +0300

    first webview with sample request

--- ZenBrowser/ViewController.swift
+++ ZenBrowser/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var browser: UIWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        self.browser.scalesPageToFit = true
+        self.browser.loadRequest(NSURLRequest(URL: NSURL(string: "http://www.kubilayerdogan.net")!))
     }
 
     override func didReceiveMemoryWarning() {
